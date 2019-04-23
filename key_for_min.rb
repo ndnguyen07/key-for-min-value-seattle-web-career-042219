a567001d677a3c14d6423ca26a7ac8952c492d4f
Nhat-Dang Nguyen <ndang.nguyen@hotmail.com>
2019-04-23 21:23:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest_value = nil
   key = nil
   name_hash.each do |one,two|
-    if smallest_value == nil two < smallest_value
+    if smallest_value == nil || two < smallest_value
    smallest_value = two
    key = one
   end
